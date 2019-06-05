d9a9ebf1c7eca8c674bacc6e33b728aee506583d
AndrewZhang <zzq.happy@qq.com>
2019-06-05 16:31:59 +0800

    MethodChannel的使用

--- android/app/src/main/java/com/example/flutter_doubanmovie/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_doubanmovie/MainActivity.java
@@ -2,16 +2,24 @@ package com.example.flutter_doubanmovie;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.widget.Toast;
+import java.util.HashMap;
+import java.util.Map;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+
+  MethodChannel mMethodChannel;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    mMethodChannel = new MethodChannel(getFlutterView(), "samples.flutter.io/message");
 
     new MethodChannel(getFlutterView(), "flutter.doubanmovie/buy").setMethodCallHandler((call, result) -> {
       switch (call.method) {
@@ -27,6 +35,42 @@ public class MainActivity extends FlutterActivity {
         default:
           result.notImplemented();
           return;
+      }
+    });
+
+    new MethodChannel(getFlutterView(), "samples.flutter.io/toast").setMethodCallHandler((call, result) -> {
+      if ("toast".equals(call.method)) {
+        if (call.hasArgument("content")) {
+          Toast.makeText(getBaseContext(), call.argument("content"), Toast.LENGTH_SHORT).show();
+          result.success("success");
+        } else {
+          result.error("-1", "toast fail", "content is null");
+        }
+      } else {
+        result.notImplemented();
+      }
+    });
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Map map = new HashMap<>();
+    map.put("content", "Android Message");
+    mMethodChannel.invokeMethod("showText", map, new MethodChannel.Result() {
+      @Override
+      public void success(Object o) {
+        Log.d("MainActivity", (String) o);
+      }
+
+      @Override
+      public void error(String s, String s1, Object o) {
+        Log.d("MainActivity", "errorCode: " + s + " errorMessage: " + s1 + " errorDetail: " + o);
+      }
+
+      @Override
+      public void notImplemented() {
+        Log.d("MainActivity", "notImplemented");
       }
     });
   }
--- lib/mine/MineWidget.dart
+++ lib/mine/MineWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MineWidget extends StatefulWidget {
   @override
@@ -6,10 +7,69 @@ class MineWidget extends StatefulWidget {
 }
 
 class _MineWidgetState extends State<MineWidget> {
+
+  static const platformChannel = const MethodChannel('samples.flutter.io/toast');
+  static const channel = const MethodChannel('samples.flutter.io/message');
+
+  String textContent = 'Flutter Message';
+
+  @override
+  void initState() {
+    super.initState();
+    channel.setMethodCallHandler((methodCall) async {
+      switch (methodCall.method) {
+        case 'showText':
+          String content = await methodCall.arguments['content'];
+          if (content != null && content.isNotEmpty) {
+            setState(() {
+              textContent = content;
+            });
+            return 'success';
+          } else {
+            throw PlatformException(
+              code: '-1',
+              message: 'showText fail',
+              details: 'content is null'
+            );
+          }
+          break;
+        default:
+          throw MissingPluginException();
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('我的'),
+    return Scaffold(
+      appBar: AppBar(title: Text('Platform Channel'),),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          RaisedButton(
+            child: Text('Show Toast'),
+            onPressed: () {
+              showToast('Flutter Toast');
+            },
+          ),
+          Text(textContent),
+        ],
+      ),
     );
+  }
+
+  void showToast(String content) async {
+    var arguments = Map();
+    arguments['content'] = content;
+
+    try {
+      String result = await platformChannel.invokeMethod('toast', arguments);
+      print('showToast ' + result);
+    } on PlatformException catch (e) {
+      print('showToast ' + e.code + e.message + e.details);
+    } on MissingPluginException catch (e) {
+      print('showToast ' + e.message);
+    }
   }
 }
